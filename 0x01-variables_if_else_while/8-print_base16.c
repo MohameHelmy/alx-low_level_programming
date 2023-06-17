68bb31030447b741067f38e3ac46f7e22bf8c160
MohameHelmy <mohamed.helmy11d@gmail.com>
2023-06-17 15:35:33 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,14 @@ int main(void)
     int i;
 
     for (i = 0; i < 10; i++)
+    {
         putchar('0' + i);
+    }
     for (i = 0; i < 6; i++)
+    {
         putchar('a' + i);
+    }
     putchar('\n');
 
-    return 0;
+    return (0);
 }
