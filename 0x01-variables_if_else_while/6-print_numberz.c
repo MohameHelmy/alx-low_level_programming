590efb61188ae6cb0542159e6329ec9f6ec9fd14
MohameHelmy <mohamed.helmy11d@gmail.com>
2023-06-16 17:23:05 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,28 +6,16 @@
  */
 int main(void)
 {
-    int num;
-
-    num = 0;
-    putchar('0' + num);
-    num++;
-    putchar('0' + num);
-    num++;
-    putchar('0' + num);
-    num++;
-    putchar('0' + num);
-    num++;
-    putchar('0' + num);
-    num++;
-    putchar('0' + num);
-    num++;
-    putchar('0' + num);
-    num++;
-    putchar('0' + num);
-    num++;
-    putchar('0' + num);
-    num++;
-    putchar('0' + num);
+    putchar('0');
+    putchar('1');
+    putchar('2');
+    putchar('3');
+    putchar('4');
+    putchar('5');
+    putchar('6');
+    putchar('7');
+    putchar('8');
+    putchar('9');
     putchar('\n');
 
     return 0;
