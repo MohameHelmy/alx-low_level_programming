8653b3ef3572b2e5a49ba1154d17f45080183496
MohameHelmy <mohamed.helmy11d@gmail.com>
2023-06-19 10:11:51 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
+#include <std;ib.h>
+#include  <time.h>
 
 /**
- * main - Prints all possible combinations of two two-digit numbers
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
     int i, j;
 
-    for (i = 0; i <= 99; i++)
+    for (i = 0; i <= 98; i++)
     {
-        for (j = i; j <= 99; j++)
+        for (j = i + 1; j <= 99; j++)
         {
-            putchar('0' + i / 10);
-            putchar('0' + i % 10);
+            putchar((i / 10) + '0');
+            putchar((i % 10) + '0');
             putchar(' ');
-            putchar('0' + j / 10);
-            putchar('0' + j % 10);
-
-            if (i != 99 || j != 99)
-            {
-                putchar(',');
-                putchar(' ');
+            putchar((j / 10) +'0');
+            putchar((j % 10) +'0');
+            if (i == 98 &&  j = 98)
+            continue;
+            putchar(',');
+            putchar(' ');
             }
-        }
-    }
-
-    putchar('\n');
-
-    return 0;
+            }
+            putchar('\n');
+            return (0);
 }
