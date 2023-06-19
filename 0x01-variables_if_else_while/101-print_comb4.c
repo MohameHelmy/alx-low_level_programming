942de3082f73a015c1475354f86ed52bd5524b6e
MohameHelmy <mohamed.helmy11d@gmail.com>
2023-06-19 09:49:39 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,19 +6,23 @@
  */
 int main(void)
 {
-    int i, j;
+    int i, j, k;
 
     for (i = 0; i <= 9; i++)
     {
         for (j = i + 1; j <= 9; j++)
         {
-            putchar('0' + i);
-            putchar('0' + j);
-
-            if (i != 8 || j != 9)
+            for (k = j + 1; k <= 9; k++)
             {
-                putchar(',');
-                putchar(' ');
+                putchar('0' + i);
+                putchar('0' + j);
+                putchar('0' + k);
+
+                if (i != 7 || j != 8 || k != 9)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
             }
         }
     }
