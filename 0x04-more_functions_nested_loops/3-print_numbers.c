75ed7544d995e24e945de9a949451f14d3805665
MohameHelmy <mohamed.helmy11d@gmail.com>
2023-07-04 15:32:51 -0700

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_numbers - Print the numbers since 0 up to 0
+*Return: The numbers since 0 up to 0
+*/
+
+void print_numbers(void)
+
+{
+int x;
+
+for (x = 0; x <= 9; x++)
+{
+putchar(x + '0');
+}
+putchar('\n');
+}
