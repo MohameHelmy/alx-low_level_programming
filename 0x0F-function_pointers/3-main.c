8b6bf4b16d7b69099d7ff36cbcbaff1c35f2bd4f
root <Mohamed.helmy11d@gmail.com>
2023-09-13 08:45:49 -0700

    your massage

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,10 +16,10 @@ int main(int argc, char *argv[])
 	int num1, num2;
 	char *op;
 
-	if (argc !=4)
+	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);;
+		exit(98);
 	}
 
 	num1 = atoi(argv[1]);
