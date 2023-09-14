0f8ca26d5c967d12e1991b017971675210b5717c
root <Mohamed.helmy11d@gmail.com>
2023-09-14 16:06:13 -0700

    n

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 
 /**
  * print_string - string to n=be printed
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,18 @@
 #ifndef _VARIADIC_H_
 #define _VARIADIC_H_
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * struct token - structs a token
+ *
+ * @token: format tokens
+ * @f: func. associated
+ */
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+} token_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
